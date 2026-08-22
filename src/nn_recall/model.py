3a9bb8781114d9aa721cd9145620408389d510fe
Evildoersudo <3597797186a@gmail.com>
2026-08-22 17:50:16 +0800

    初步完成了nn recall代码的编写

---
+++ src/nn_recall/model.py
@@ -0,0 +1,29 @@
+"""
+              Session历史
+                  |
+                  |
+                  ↓
+          Session Encoder
+                  |
+                  |
+                  ↓
+          session embedding
+             (300维)
+
+
+                         cosine similarity
+
+
+             item embedding
+                  ↑
+                  |
+              aid embedding
+
+
+                  |
+                  ↓
+
+              TopK items
+Encoder网络
+"""
+
